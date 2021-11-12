963754d8951c6fb83ccd9f37cf10e81ad1c2c70d
Isabel Foo <isabel.foo@hotmail.com>
2021-11-12 15:15:47 +0000

    upload image function added for creating new list

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -7,9 +7,9 @@ class BookmarksController < ApplicationController
   def create
     @bookmark = Bookmark.new(bookmark_params)
     @list = List.find(params[:list_id])
-    @movie = Movie.find(@bookmark[:movie_id])
+    # @movie = Movie.find(@bookmark[:movie_id])
     @bookmark.list = @list
-    @bookmark.movie = @movie
+    # @bookmark.movie = @movie
     if @bookmark.save
       redirect_to list_path(@list)
     else
@@ -27,7 +27,7 @@ class BookmarksController < ApplicationController
   private
 
   def bookmark_params
-    params.require(:bookmark).permit(:comment, :movie_id, :list_id)
+    params.require(:bookmark).permit(:comment, :movie_id)
   end
 
 end
